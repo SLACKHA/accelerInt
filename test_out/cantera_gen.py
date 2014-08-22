597309713fede32cf7122817565bfbe2e3dec169
Nick-lab <nicholas.curtis@uconn.edu>
2014-08-22 14:15:38 -0400

    updated to have more similar conditions next to each other
    Signed-off-by: Nick-lab <nicholas.curtis@uconn.edu>

--- test_out/cantera_gen.py
+++ test_out/cantera_gen.py
@@ -1,121 +1,61 @@
 import cantera as ct
 import numpy as np
-import multiprocessing as mp
 
-def listener(q):
-    '''listens for messages on the q, writes to file. '''
-    with open("ign_data.txt", "w+") as file:
-        while 1:
-            m = q.get()
-            if m == 'kill':
-                break
-            file.write(m + '\n')
-            file.flush()
-
-samples = 200.0
-t_end = 100
-size = int(1e6)
+t_end = 10
 def main():
-    manager = mp.Manager()
-    q = manager.Queue()
-    THREADS = 12
-    pool = mp.Pool(THREADS + 1)
-
-    #put listener to work first
-    watcher = pool.apply_async(listener, (q,))
-
-    #conditions
-    T_start = np.arange(760.0, 2500.0, 50.0)
-    P_start = np.arange(101325.0, 50.0 * 101325.0, 5.0 * 101325.0)
-    Phi_start = np.arange(0.5, 3.5, 0.1)
-
-    jobs = []
-    for T in T_start:
-        for P in P_start:
-            for Phi in Phi_start:
-                job = pool.apply_async(run_sim, (T, P, Phi, q))
-                jobs.append(job)
-
-    # collect results from the workers through the pool result queue
-    my_sum = 0
-    for i, job in enumerate(jobs):
-        my_sum += job.get()
-        print "Job #" + str(i) + " of " + str(len(jobs)) + " complete."
-    print "# of Conditions = " + str(my_sum * samples)
-
-    #now we are done, kill the listener
-    q.put('kill')
-    pool.close()
-
-def run_sim(T, P, Phi, q):
     gas = ct.Solution("h2.xml")
-    t = 0
-    ignitions = 0
+    with open("ign_data.txt", "w+") as file:
+        #conditions
+        T_start = np.arange(900.0, 2500.0, 25.0)
+        P_start = np.arange(101325.0, 50.0 * 101325.0, 5.0 * 101325.0)
+        Phi_start = np.arange(1.0, 3.0, 1.0)
 
+        my_sum = 0
+
+        runs = len(T_start) * len(P_start) * len(Phi_start)
+        run = 0
+        for T in T_start:
+            for P in P_start:
+                for Phi in Phi_start:
+                    print "Run: {:} of {:}".format(run + 1, runs)
+                    run += 1 
+                    my_sum += run_sim(T, P, Phi, file, gas)
+        print "# of Conditions = " + str(my_sum)
+
+def run_sim(T, P, Phi, file, gas):
     #state array for each simulation state, and each mass fraction + T & P
-    state_array = np.zeros((size, gas.n_species + 2))
     state = np.zeros(gas.n_species + 2)
+    states = 0
 
     #constant pressure run
     gas.TPX = T,P, "H2:" + str(Phi) + ", O2:1.0, N2:3.76"
     reac = ct.IdealGasConstPressureReactor(gas)
     net = ct.ReactorNet([reac])
 
-    i = 0
     t = 0
-    while t < t_end and i < size:
+    while t < t_end:
         state[0] = reac.T
         state[1] = reac.thermo.P 
         state[2:] = reac.Y[:]
-        state_array[i, :] = state[:]
+        file.write(' '.join(np.char.mod('%f', state[:])) + "\n")
         t = net.step(t_end)
-        i += 1
-
-    #ok, now get samples based on equidistant T
-    T_0 = state_array[0, 0]
-    T_end = state_array[i - 1, 0]
-    if T_end > T_0 + 400:
-        ignitions += 1
-        delta_T = (T_end - T_0) / samples
-        T_next = T_0
-        for i in range(size):
-            if state_array[i, 0] >= T_next:
-                arr_str = np.char.mod('%f', state_array[i, :])
-                q.put(' '.join(arr_str))
-                T_next += delta_T
-            if T_next > T_end:
-                break
+        states += 1
 
     #conv run
     gas.TPX = T,P, "H2:" + str(Phi) + ", O2:1.0, N2:3.76"
     reac = ct.IdealGasReactor(gas)
     net = ct.ReactorNet([reac])
 
-    i = 0
-    t = 0
-    while t < t_end and i < size:
+    while t < t_end:
         state[0] = reac.T
         state[1] = reac.thermo.P 
         state[2:] = reac.Y[:]
         state_array[i, :] = state[:]
+        file.write(' '.join(np.char.mod('%f', state[:])) + "\n")
         t = net.step(t_end)
-        i += 1
+        states += 1
 
-    #ok, now get samples based on equidistant T
-    T_0 = state_array[0, 0]
-    T_end = state_array[i - 1, 0]
-    if T_end > T_0 + 400:
-        ignitions += 1
-        delta_T = (T_end - T_0) / samples
-        T_next = T_0
-        for i in range(size):
-            if state_array[i, 0] >= T_next:
-                arr_str = np.char.mod('%f', state_array[i, :])
-                q.put(' '.join(arr_str))
-                T_next += delta_T
-            if T_next > T_end:
-                break
-    return ignitions
+    return states
                     
 
 
