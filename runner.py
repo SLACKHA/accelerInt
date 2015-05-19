46738edf2341e379fa775f33c95529b84ec567a4
Nick-lab <nicholas.curtis@uconn.edu>
2015-05-19 09:43:12 -0400

    major updates to the runner, should automatically do repeated trials of both shuffled and unshuffled data now

--- runner.py
+++ runner.py
@@ -1,70 +1,109 @@
 #! /usr/bin/env python2.7
 import math
 import subprocess
-import os, glob
+import os, glob, shutil
 import sys
 from argparse import ArgumentParser
 
-parser = ArgumentParser(description='Runs all integrators for the given mechanism / options')
-parser.add_argument('-diff', '--different_ics',
-					default=False,
-					action = 'store_true',
-					required=False,
-					help = 'Use different initial conditions')
-parser.add_argument('-sh', '--shuffle',
-					default=False,
-					action='store_true',
-					required=False,
-					help = 'Shuffle the ics, implies --different-ics')
-args = parser.parse_args()
-maker = ['make', '-j24', 'DEBUG=FALSE', 'IGN=TRUE', 'PRINT=FALSE',  'LOG_OUTPUT=FALSE']
-if args.shuffle:
-	maker.extend(["SAME_IC=FALSE", "SHUFFLE=TRUE"])
-elif args.different_ics:
-	maker.extend(["SAME_IC=FALSE"])
+def check_dir(dir, force):
+	old_files = [file for file in os.listdir(dir) if '.timing' in file and os.path.isfile(os.path.join(dir, file))]
+	if len(old_files) and not force:
+		raise Exception("Old data found in /{}/... stopping".format(dir))
 
-all_exes = []
-for file in glob.glob('*-int*'):
-	all_exes.append(file)
+def make_sure_path_exists(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
 
-all_exes = sorted(all_exes, key = lambda x: 1000 if 'gpu' in x else 1)
+def run(name, diff=False, shuffle=False, force=False):
+	maker = ['make', '-j24', 'DEBUG=FALSE', 'IGN=TRUE', 'PRINT=FALSE',  'LOG_OUTPUT=FALSE']
+	if shuffle:
+		maker.extend(["SAME_IC=FALSE", "SHUFFLE=TRUE"])
+	elif different_ics:
+		maker.extend(["SAME_IC=FALSE"])
 
-threads = [12]
-powers = [0, 3, 7, 10, 14, 17, 20]
-l_t = [False, True]
-l_s = [False, True]
-exppowers = [int(pow(2, exponent)) for exponent in powers]
+	with open('src/launch_bounds.cuh') as file:
+		lines = [line.strip() for line in lines]
+		block_size = next(line for line in lines if 'TARGET_BLOCK_SIZE' in line)
+		block_size = block_size[block_size.index('(') + 1 : block_size.index(')')]
 
-for low_tol in l_t:
-	for large_step in l_s:
-		flags = maker[:]
-		if low_tol:
-			flags += ['LOW_TOL=TRUE', 'FAST_MATH=TRUE']
-		else:
-			flags += ['FAST_MATH=FALSE']
-		if large_step:
-			flags += ['LARGE_STEP=TRUE']
-		#force remake
-		subprocess.call(flags)
-		for exe in all_exes:
-			for thread in threads:
-				for i in range(len(powers)):
-					if powers[i] > 14 and not args.different_ics and not args.shuffle:
-						continue
-					if ('exp' in exe and 'gpu' in exe) and not low_tol:
-						continue
-					elif ('exp' in exe and 'gpu' in exe) and powers[i] > 10:
-						continue
-					elif ('exp' in exe and not 'gpu' in exe) and powers[i] > 14:
-						continue
-					filename = './output/' + exe + '_' + str(thread) +'_'+str(powers[i]) + ('_lt' if low_tol else '') \
-								+ ('_ls' if large_step else '') +'.txt'
-					if (os.path.isfile(filename)):
-						continue
-					file = open(filename, "w")
-					if 'gpu' in filename:
-						subprocess.call([os.path.join(os.getcwd(), exe), str(exppowers[i])], stdout=file)
-					else:
-						subprocess.call([os.path.join(os.getcwd(), exe), str(thread), str(exppowers[i])], stdout=file)
-					file.flush()
-					file.close()
+	all_exes = []
+	for file in glob.glob('*-int*'):
+		all_exes.append(file)
+
+	all_exes = sorted(all_exes, key = lambda x: 1000 if 'gpu' in x else 1)
+
+	threads = [6, 12]
+	powers = [0, 3, 7, 10, 14, 17, 20]
+	Repeats = 10
+	l_t = [False, True]
+	l_s = [False, True]
+	exppowers = [int(pow(2, exponent)) for exponent in powers]
+
+	#make sure we don't have old data
+	check_dir('output', force)
+	if not shuffle and not diff:
+		desc = '_same_ic'
+	elif shuffle:
+		desc = '_shuffled_ic'
+	elif diff:
+		desc = '_diff_ic'
+	else:
+		raise Exception('Cannot turn on Shuffle and Diff at same time')
+
+	make_sure_path_exists(name + desc)
+	results_folder = os.path.abspath(name + desc)
+	check_dir(results_folder, force)
+
+	file_list = []
+	for repeat in range(repeats):
+		for low_tol in l_t:
+			for large_step in l_s:
+				flags = maker[:]
+				if low_tol:
+					flags += ['LOW_TOL=TRUE', 'FAST_MATH=TRUE']
+				else:
+					flags += ['FAST_MATH=FALSE']
+				if large_step:
+					flags += ['LARGE_STEP=TRUE']
+				#force remake
+				subprocess.call(flags)
+				for exe in all_exes:
+					the_threads = threads if not 'gpu' in exe else [block_size]
+					for thread in threads:
+						for i in range(len(powers)):
+							if powers[i] > 14 and not different_ics and not shuffle:
+								continue
+							if 'exp' in exe: 
+								continue
+							filename = './output/' + exe + '_' + str(thread) +'_' + str(powers[i]) + ('_lt' if low_tol else '') \
+										+ ('_ls' if large_step else '') +'.timing'
+							if filename not in file_list:
+								file_list.append(filename)
+							mode = 'w' if repeat == 0 else 'a'
+							file = open(filename, mode)
+							if 'gpu' in filename:
+								subprocess.call([os.path.join(os.getcwd(), exe), str(exppowers[i])], stdout=file)
+							else:
+								subprocess.call([os.path.join(os.getcwd(), exe), str(thread), str(exppowers[i])], stdout=file)
+							file.flush()
+							file.close()
+	#finally move the files so we can run other cases
+	for file in file_list:
+		shutil.move(os.path.join('output', file), results_folder, file)
+
+if __name__ == '__main__':
+	parser = ArgumentParser(description='Runs timing runs for the various integrators')
+	parser.add_arguement('-n', '--name',
+						type=str,
+						required=True,
+						help='the name of the mechanism')
+	parser.add_arguement('-f', '--force'
+						required=False,
+						default=False,
+						action='store_true',
+						help='Force reuse of past data files')
+	run(args.name, diff=True, shuffle=False, args.force)
+	run(args.name, diff=False, shuffle=True, args.force)
