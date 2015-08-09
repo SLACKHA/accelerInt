6eca6ba73f0ef06901991d8e306054ca956e8cf7
Nick Curtis <arghdos@gmail.com>
2015-08-09 14:40:49 -0400

    updated to new rates headers

--- src/derivs.cuh
+++ src/derivs.cuh
@@ -2,9 +2,7 @@
 #define DERIVS_HEAD_DEVICE
 
 #include "header.h"
-
-__device__ void dydt (const Real, const Real, const Real*, Real*);
-__device__ void eval_jacob (const Real, const Real, const Real*, Real*);
-__device__ void eval_fd_jacob (const Real, const Real, Real *, Real *);
+#include "dydt.cuh"
+#include "jacob.cuh"
 
 #endif
--- src/derivs.h
+++ src/derivs.h
@@ -2,9 +2,7 @@
 #define DERIVS_HEAD
 
 #include "header.h"
-
-void dydt (Real t, Real pres, Real* y, Real* dy);
-void eval_jacob (Real t, Real pres, Real* y, Real* jac);
-void eval_fd_jacob (const Real t, const Real pres, Real * y, Real * jac);
+#include "dydt.h"
+#include "jacob.h"
 
 #endif
