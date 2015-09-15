004bc78a105ffd62c1541b3af360660d9c451a03
arghdos <arghdos@gmail.com>
2015-09-15 10:07:49 -0400

    use full path

--- runner.py
+++ runner.py
@@ -175,8 +175,8 @@ if __name__ == '__main__':
 
     home = os.getcwd()
     a_dir = os.path.join(os.getcwd(), args.base_dir)
-    dir_list = [os.path.join(a_dir, name) for name in os.listdir(a_dir)
-            if os.path.isdir(os.path.join(a_dir, name))]
+    dir_list = sorted([os.path.join(a_dir, name) for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))])
 
     for d in dir_list:
         os.chdir(d)
