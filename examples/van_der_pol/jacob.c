f5069607780e781cc19d0b933ab6b4a5189959c6
Nick <nicholas.curtis@uconn.edu>
2018-05-03 12:59:18 -0400

    correct VDP docs

--- examples/van_der_pol/jacob.c
+++ examples/van_der_pol/jacob.c
@@ -13,29 +13,16 @@
 namespace van_der_pol {
 #endif
 
-/**
- * \brief An implementation of the van der Pol jacobian
- *
- * \param[in]           t               The current system time
- * \param[in]           mu              The van der Pol parameter
- * \param[in]           y               The state vector at time t
- * \param[out]          jac             The jacobian to populate
- *
- *  The Jacobian is in a local Column-major (Fortran) order.  As with dydt(), this function operates on local
- *  copies of the global state vector and jacobian.  Hence simple linear indexing can be used here.
- *  @see solver_generic.c
- *
- */
 void eval_jacob (const double t, const double mu, const double * __restrict__ y, double * __restrict__ jac)
 {
     //Note, to reach index [i, j] of the Jacobian, we multiply `i` by NSP, the size of the first dimension of Jacobian and add j, i.e.:
     //jac[i, j] -> jac[i * NSP + j]
     //!jac[0, 0] = \f$\frac{\partial \dot{y_1}}{\partial y_1}\f$
     jac[0 * NSP + 0] = 0;
-    //!jac[0, 1] = \f$\frac{\partial \dot{y_2}}{\partial y_1}\f$
-    jac[0 * NSP + 1] = -2 * mu * y[0] * y[1] - 1;
-    //!jac[1, 0] = \f$\frac{\partial \dot{y_1}}{\partial y_2}\f$
-    jac[1 * NSP + 0] = 1;
+    //!jac[0, 1] = \f$\frac{\partial \dot{y_1}}{\partial y_2}\f$
+    jac[0 * NSP + 1] = 1;
+    //!jac[1, 0] = \f$\frac{\partial \dot{y_2}}{\partial y_1}\f$
+    jac[1 * NSP + 0] = -2 * mu * y[0] * y[1] - 1;
     //!jac[1, 1] = \f$\frac{\partial \dot{y_2}}{\partial y_2}\f$
     jac[1 * NSP + 1] = mu * (1 - y[0] * y[0]);
 }
