7c627149870ec1909dcbe2778916b980d8773144
Nick <nicholas.curtis@uconn.edu>
2018-09-22 12:25:14 -0400

    convert exprb43

---
+++ exponential_integrators/exprb43/exprb43.cpp
@@ -0,0 +1,287 @@
+/*!
+ * \file exprb43.c
+ *
+ * \author Nicholas J. Curtis
+ * \date 09/02/2014
+ *
+ * \brief A krylov subspace integrator using a 4th order (3rd-order embedded)
+ * 		  exponential Rosenbrock method of Hochbruck et al. (2009)
+ *
+ * See full reference:
+ * M. Hochbruck, A. Ostermann, J. Schweitzer, Exponential Rosenbrock-type methods, SIAM J. Numer. Anal. 47 (1) (2009) 786–803. doi:10.1137/080717717.
+ *
+ * NOTE: all matricies stored in column major format!
+ *
+ */
+
+/** Include common code. */
+#include <cmath>
+#include <cstring>
+
+#include "dydt.h"
+#include "jacob.h"
+#include "exprb43_solver.hpp"
+
+namespace c_solvers {
+
+
+	///////////////////////////////////////////////////////////////////////////////
+
+	/*!
+	 * \fn int integrate(const double t_start, const double t_end, const double pr, double* y)
+	 * \param t_start The initial integration time
+	 * \param t_end The final integration timestep
+	 * \param pr User data passed to the RHS function dydt() - commonly used for the Pressure term
+	 * \param y The state vector
+	 * \brief 4th-order exponential integrator function w/ adaptive Kyrlov subspace approximation
+	 * \returns The result of this integration step @see exprb43_ErrCodes
+	 */
+	ErrorCode integrate (const double t_start, const double t_end, const double pr, double* __restrict__ y) {
+
+		//initial time
+	#ifdef CONST_TIME_STEP
+		double h = t_end - t_start;
+	#else
+		double h = fmin(1.0e-8, t_end - t_start);
+	#endif
+		double h_new;
+
+		double err_old = 1.0;
+		double h_old = h;
+
+		bool reject = false;
+		int failures = 0;
+		int steps = 0;
+
+		double t = t_start;
+
+		int tid = omp_get_thread_num();
+
+		// get scaling for weighted norm
+		double* __restrict__ sc = _unique<double>(tid, _sc);
+		scale_init(y, sc);
+
+		double beta = 0;
+
+		// temporary arrays
+		double* __restrict__ temp = _unique<double>(tid, _temp);
+		double* __restrict__ f_temp = _unique<double>(tid, _ftemp);
+		double* __restrict__ y1 = _unique<double>(tid, _y1);
+
+		// source vector
+		double* __restrict__ fy = _unique<double>(tid, _fy);
+
+		// Jacobian matrix
+		double* __restrict__ A = _unique<double>(tid, _A);
+		double* __restrict__ gy = _unique<double>(tid, _gy);
+
+		double* __restrict__ Hm = _unique<double>(tid, _Hm);
+		double* __restrict__ Vm = _unique<double>(tid, _Vm);
+		double* __restrict__ phiHm = _unique<double>(tid, _phiHm);
+		double err = 0.0;
+		double* __restrict__ savedActions = _unique<double>(tid, _savedActions);
+		int numSteps = 0;
+		while ((t < t_end) && (t + h > t)) {
+
+			//error checking
+			if (failures >= 5)
+			{
+				return ErrorCode::MAX_CONSECUTIVE_ERRORS_EXCEEDED;
+			}
+			if (steps++ >= MAX_STEPS)
+			{
+				return ErrorCode::MAX_STEPS_EXCEEDED;
+			}
+			if (t + h <= t)
+			{
+				return ErrorCode::H_PLUS_T_EQUALS_H;
+			}
+
+			if (!reject) {
+				dydt (t, pr, y, fy);
+				eval_jacob (t, pr, y, A);
+				//gy = fy - A * y
+				sparse_multiplier(A, y, gy);
+
+				for (int i = 0; i < NSP; ++i) {
+					gy[i] = fy[i] - gy[i];
+				}
+			}
+
+			//do arnoldi
+			int m = arnoldi(0.5, 1, h, A, fy, sc, &beta, Vm, Hm, phiHm);
+			if (m + 1 >= STRIDE || m < 0)
+			{
+				//need to reduce h and try again
+				h /= 5.0;
+				failures++;
+				reject = true;
+				continue;
+			}
+
+			// Un2 to be stored in temp
+			//Un2 is partially in the mth column of phiHm
+			//Un2 = y + ** 0.5 * h * phi_1(0.5 * h * A)*fy **
+			//Un2 = y + ** beta * Vm * phiHm(:, m) **
+
+			//store h * beta * Vm * phi_1(h * Hm) * e1 in savedActions
+			matvec_m_by_m_plusequal(m, phiHm, &phiHm[m * STRIDE], temp);
+			matvec_n_by_m_scale(m, beta, Vm, temp, savedActions);
+
+			//store 0.5 * h *  beta * Vm * phi_1(0.5 * h * Hm) * fy + y in temp
+			matvec_n_by_m_scale_add(m, beta, Vm, &phiHm[m * STRIDE], temp, y);
+			//temp is now equal to Un2
+
+			//next compute Dn2
+			//Dn2 = (F(Un2) - Jn * Un2) - gy
+
+			dydt(t, pr, temp, &savedActions[NSP]);
+			sparse_multiplier(A, temp, f_temp);
+
+
+			for (int i = 0; i < NSP; ++i) {
+				temp[i] = savedActions[NSP + i] - f_temp[i] - gy[i];
+			}
+			//temp is now equal to Dn2
+
+			//partially compute Un3 as:
+			//Un3 = y + ** h * phi_1(hA) * fy ** + h * phi_1(hA) * Dn2
+			//Un3 = y + ** h * beta * Vm * phiHm(:, m) **
+
+			//now we need the action of the exponential on Dn2
+			int m1 = arnoldi(1.0, 4, h, A, temp, sc, &beta, Vm, Hm, phiHm);
+			if (m1 + 4 >= STRIDE || m1 < 0)
+			{
+				//need to reduce h and try again
+				h /= 5.0;
+				failures++;
+				reject = true;
+				continue;
+			}
+
+			//save Phi3(h * A) * Dn2 to savedActions[0]
+			//save Phi4(h * A) * Dn2 to savedActions[NSP]
+			//add the action of phi_1 on Dn2 to y and hn * phi_1(hA) * fy to get Un3
+			const double* in[5] = {&phiHm[(m1 + 2) * STRIDE], &phiHm[(m1 + 3) * STRIDE], &phiHm[m1 * STRIDE], savedActions, y};
+			double* out[3] = {&savedActions[NSP], &savedActions[2 * NSP], temp};
+			double scale_vec[3] = {beta / (h * h), beta / (h * h * h), beta};
+			matvec_n_by_m_scale_special(m1, scale_vec, Vm, in, out);
+			//Un3 is now in temp
+
+			//next compute Dn3
+			//Dn3 = F(Un3) - A * Un3 - gy
+			dydt(t, pr, temp, &savedActions[3 * NSP]);
+			sparse_multiplier(A, temp, f_temp);
+
+
+			for (int i = 0; i < NSP; ++i) {
+				temp[i] = savedActions[3 * NSP + i] - f_temp[i] - gy[i];
+			}
+			//temp is now equal to Dn3
+
+			//finally we need the action of the exponential on Dn3
+			int m2 = arnoldi(1.0, 4, h, A, temp, sc, &beta, Vm, Hm, phiHm);
+			if (m2 + 4 >= STRIDE || m2 < 0)
+			{
+				//need to reduce h and try again
+				h /= 5.0;
+				failures++;
+				reject = true;
+				continue;
+			}
+
+			out[0] = &savedActions[3 * NSP];
+			out[1] = &savedActions[4 * NSP];
+			in[0] = &phiHm[(m2 + 2) * STRIDE];
+			in[1] = &phiHm[(m2 + 3) * STRIDE];
+			scale_vec[0] = beta / (h * h);
+			scale_vec[1] = beta / (h * h * h);
+			matvec_n_by_m_scale_special2(m2, scale_vec, Vm, in, out);
+
+			//construct y1 and error vector
+
+			for (int i = 0; i < NSP; ++i) {
+				//y1 = y + h * phi1(h * A) * fy + h * sum(bi * Dni)
+				y1[i] = y[i] + savedActions[i] + 16.0 * savedActions[NSP + i] - 48.0 * savedActions[2 * NSP + i] + -2.0 * savedActions[3 * NSP + i] + 12.0 * savedActions[4 * NSP + i];
+				//error vec
+				temp[i] = 48.0 * savedActions[2 * NSP + i] - 12.0 * savedActions[4 * NSP + i];
+			}
+
+
+	#ifndef CONST_TIME_STEP
+			//scale and find err
+			scale (y, y1, f_temp);
+			err = fmax(EPS, sc_norm(temp, f_temp));
+
+			// classical step size calculation
+			h_new = pow(err, -1.0 / ORD);
+
+			failures = 0;
+			if (err <= 1.0) {
+
+				#ifdef LOG_KRYLOV_AND_STEPSIZES
+					fprintf (logFile, "%.15le\t%.15le\t%.15le\t%d\t%d\t%d\n", t, h, err, m, m1, m2);
+	  			#endif
+
+				memcpy(sc, f_temp, NSP * sizeof(double));
+
+				// minimum of classical and Gustafsson step size prediction
+				h_new = fmin(h_new, (h / h_old) * pow((err_old / (err * err)), (1.0 / ORD)));
+
+				// limit to 0.2 <= (h_new/8) <= 8.0
+				h_new = h * fmax(fmin(0.9 * h_new, 8.0), 0.2);
+
+				// update y and t
+
+				for (int i = 0; i < NSP; ++i) {
+					y[i] = y1[i];
+				}
+
+				t += h;
+
+				// store time step and error
+				err_old = fmax(1.0e-2, err);
+				h_old = h;
+
+				// check if last step rejected
+				if (reject) {
+					reject = false;
+					h_new = fmin(h, h_new);
+				}
+				h = fmin(h_new, t_end - t);
+				numSteps++;
+
+			} else {
+
+				#ifdef LOG_KRYLOV_AND_STEPSIZES
+					fprintf (rFile, "%.15le\t%.15le\t%.15le\t%d\t%d\t%d\n", t, h, err, m, m1, m2);
+	  			#endif
+
+				// limit to 0.2 <= (h_new/8) <= 8.0
+				h_new = h * fmax(fmin(0.9 * h_new, 8.0), 0.2);
+				h_new = fmin(h_new, t_end - t);
+
+				reject = true;
+				h = fmin(h, h_new);
+			}
+	#else
+			//constant time stepping
+			// update y and t
+			for (int i = 0; i < NSP; ++i) {
+				y[i] = y1[i];
+			}
+
+			t += h;
+	#endif
+
+		} // end while
+
+		#ifdef LOG_KRYLOV_AND_STEPSIZES
+			fclose(logFile);
+			fclose(rFile);
+		#endif
+
+		return ErrorCode::SUCCESS;
+	}
+
+}
---
+++ exponential_integrators/exprb43/exprb43_solver.hpp
@@ -0,0 +1,145 @@
+/*!
+ * \file exprb43_solver.hpp
+ * \brief Definition of the CPU EXPRB43 algorithm
+ * \author Nicholas Curtis
+ * \date 03/10/2015
+ *
+ * modified 09/22 for conversion to c++
+ */
+
+#ifndef EXPRB43_SOLVER_HPP
+#define EXPRB43_SOLVER_HPP
+
+#include "exp_solver.hpp"
+
+namespace c_solvers
+{
+
+    //if defined, uses (I - h * Hm)^-1 to smooth the krylov error vector
+    //#define USE_SMOOTHED_ERROR
+     //! max order of the phi functions (for error estimation)
+    #define P 4
+    //! order of embedded methods
+    #define ORD 3.0
+    //! Maximum allowed internal timesteps per integration step
+    #define MAX_STEPS (100000)
+    //! Number of consecutive errors on internal integration steps allowed before exit
+    #define MAX_CONSECUTIVE_ERRORS (5)
+
+
+    class EXPRB43Integrator : public ExponentialIntegrator
+    {
+    protected:
+        // log format t, h, err, m, m1, m2
+        std::vector<std::tuple<double, double, double, int, int, int>> subspaceLog;
+
+        //! offsets
+
+        //! offset for scaling vector
+        std::size_t _sc;
+        // source vector offset
+        std::size_t _fy;
+        // linear approximant vector offset
+        std::size_t _gy;
+        // Jacobian matrix offset
+        std::size_t _A;
+        // temporary array offsets
+        std::size_t _temp;
+        std::size_t _ftemp;
+        std::size_t _y1;
+        std::size_t _Hm;
+        std::size_t _Vm;
+        std::size_t _phiHm;
+
+        // action vector offset
+        std::size_t _savedActions;
+
+    public:
+
+        EXPRB43Integrator(int neq, int numThreads,
+                          double atol=1e-10, double rtol=1e-6,
+                          int N_RA=10, int M_MAX=-1) :
+            ExponentialIntegrator(neq, numThreads, M_MAX + P, atol, rtol, N_RA, M_MAX)
+        {
+
+        }
+
+        /*!
+           \fn char* solverName()
+           \brief Returns a descriptive solver name
+        */
+        const char* solverName() const {
+            const char* name = "exprb43-int";
+            return name;
+        }
+
+        void clean() {
+            // pass
+        }
+        void reinitialize(int numThreads) {
+            // pass
+        }
+
+        void initSolverLog() {
+            // pass
+        }
+
+        void solverLog() {
+            // pass
+        }
+
+        std::size_t requiredSolverMemorySize()
+        {
+            std::size_t working = ExponentialIntegrator::requiredSolverMemorySize();
+            // sc
+            _sc = working;
+            working += _neq * sizeof(double);
+            // fy
+            _fy = working;
+            working += _neq * sizeof(double);
+            // gy
+            _gy = working;
+            working += _neq * sizeof(double);
+            // A
+            _A = working;
+            working += _neq * _neq * sizeof(double);
+            // temp
+            _temp = working;
+            working += _neq * sizeof(double);
+            // _ftemp
+            _ftemp = working;
+            working += _neq * sizeof(double);
+            // y1
+            _y1 = working;
+            working += _neq * sizeof(double);
+            // _Hm
+            _Hm = working;
+            working += STRIDE * STRIDE * sizeof(double);
+            // _Vm
+            _Vm = working;
+            working += _neq * STRIDE * sizeof(double);
+            // _phiHm
+            _phiHm = working;
+            working += STRIDE * STRIDE * sizeof(double);
+            // k1
+            _savedActions = working;
+            working += 5 * _neq * sizeof(double);
+            return working;
+        }
+
+        /**
+         * \brief Driver function for RKC integrator.
+         *
+         * \param[in,out] t     The time (starting, then ending).
+         * \param[in] tEnd      The desired end time.
+         * \param[in] pr        A parameter used for pressure or density to pass to the derivative function.
+         * \param[in,out] y     Dependent variable array, integrated values replace initial conditions.
+         */
+        ErrorCode integrate (
+            const double t_start, const double t_end, const double pr, double* y);
+
+    };
+
+}
+
+#endif
