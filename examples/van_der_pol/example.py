fdb4d9121349a7efa543130065b4379b30d785da
Nick <nicholas.curtis@uconn.edu>
2018-09-24 19:47:27 -0400

    cleanup

--- examples/van_der_pol/example.py
+++ examples/van_der_pol/example.py
@@ -4,30 +4,46 @@
 
 import numpy as np
 import sys
+import multiprocessing
 import os
+import argparse
 sys.path.insert(0, os.getcwd())
 import pyccelerInt_cpu as pycel
 
-# set number of IVPs to solve
-num = 100
 
-# number of threads
-num_threads = 4
+def run(num, num_threads):
+    # number of equations
+    neq = 2
 
-# number of equations
-neq = 2
+    # create state vectors
+    phi = 2 * np.random.random((num, 2))
 
-# create state vectors
-phi = 2 * np.random.random((num, 2))
+    # set parameters
+    params = 5 * np.random.random(num)
 
-# set parameters
-params = 5 * np.random.random(num)
+    # create the integrator
+    integrator = pycel.PyIntegrator(pycel.IntegratorType.CVODES, neq, num_threads)
 
-# create the integrator
-integrator = pycel.PyIntegrator(pycel.IntegratorType.CVODES, neq, num_threads)
+    # and integrate
+    time = integrator.integrate(num, 0., 10., phi.flatten('F'), params.flatten('F'),
+                                step=1.)
 
-# and integrate
-time = integrator.integrate(num, 0., 10., phi.flatten('F'), params.flatten('F'),
-                            step=1.)
+    print('Integration completed in {} (ms)'.format(time))
 
-print('Integration completed in {} (ms)'.format(time))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('example.py - run the van der Pol accelerInt '
+                                     'example')
+    parser.add_argument('-ni', '--num_ivp',
+                        type=int,
+                        default=100,
+                        help='The number of IVPs to solve [default: 100].')
+
+    parser.add_argument('-nt', '--num_threads',
+                        type=int,
+                        default=multiprocessing.cpu_count(),
+                        help='The number of threads to use during solution '
+                             '[default: # of logical cores].')
+
+    args = parser.parse_args()
+    run(args.num_ivp, args.num_threads)
