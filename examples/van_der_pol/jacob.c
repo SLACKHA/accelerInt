5a159a74aac1d90c87f5570d10d0f9191e2258a4
Nick <nicholas.curtis@uconn.edu>
2018-09-26 11:06:26 -0400

    I think this is right?

--- examples/van_der_pol/jacob.c
+++ examples/van_der_pol/jacob.c
@@ -23,23 +23,23 @@ namespace van_der_pol {
 void eval_jacob (const double t, const double mu, const double * __restrict__ y, double * __restrict__ jac)
 {
     //! \note To get the value at index [i, j] of the Jacobian,
-    //! we multiply `i` by NSP,
-    //! the size of the first dimension of Jacobian and add j, i.e.:
-    //! jac[i, j] -> jac[i * NSP + j].
+    //! we multiply `j` by NSP, the size of the first dimension of Jacobian
+    //! and add `j`, i.e.:
+    //! jac[i, j] -> jac[i + j * NSP].
     //!
     //! Remember that the Jacobian is flatten in column-major order, hence:
     //! \code
     //!     jac = [jac[0, 0], jac[1, 0], jac[0, 1], jac[1, 1]]
     //! \endcode
-    
+
     //!jac[0, 0] = \f$\frac{\partial \dot{y_1}}{\partial y_1}\f$
-    jac[0 * NSP + 0] = 0;
+    jac[0 + 0 * NSP] = 0;
     //!jac[1, 0] = \f$\frac{\partial \dot{y_2}}{\partial y_1}\f$
-    jac[1 * NSP + 0] = -2 * mu * y[0] * y[1] - 1;
+    jac[1 + 0 * NSP] = -2 * mu * y[0] * y[1] - 1;
     //!jac[0, 1] = \f$\frac{\partial \dot{y_1}}{\partial y_2}\f$
-    jac[1 * NSP + 0] = 1;
+    jac[0 + 1 * NSP] = 1;
     //!jac[1, 1] = \f$\frac{\partial \dot{y_2}}{\partial y_2}\f$
-    jac[1 * NSP + 1] = mu * (1 - y[0] * y[0]);
+    jac[1 + 1 * NSP] = mu * (1 - y[0] * y[0]);
 }
 
 #ifdef GENERATE_DOCS
